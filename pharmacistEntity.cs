c6e0f5da11a61aeee40d8e95f7e544f79f35cb6c
Zeyad Sherif <125796653+zeyadsheriif@users.noreply.github.com>
2024-06-15 21:18:01 +0300

    Create pharmacistEntity.cs

---
+++ pharmacistEntity.cs
@@ -0,0 +1,10 @@
+namespace DatabaseProject.Models
+{
+    public class pharmacistEntity
+    {
+        public int ID { get; set; }
+        public string profile { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+    }
+}
